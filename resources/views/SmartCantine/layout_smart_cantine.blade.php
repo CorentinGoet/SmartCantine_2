c24699bddda7a05e28318b4f23d5e352f5f60089
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-12-07 17:05:24 +0100

    changed stuff

--- resources/views/SmartCantine/layout_smart_cantine.blade.php
+++ resources/views/SmartCantine/layout_smart_cantine.blade.php
@@ -7,9 +7,17 @@
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
 </head>
 <body>
-    <header>
-        test
-    </header>
+    <div class="header">
+        <h2>
+            SmartCantine
+        </h2>
+        <ul><!--c'est unorderd list -->
+            <li> <a href="test 1.html" target="_blank" title="go to test">Home</a> </li>
+            <li> <a href="About.html" target="_blank" title="go to About">About</a> </li>
+            <li><a href="Services.html" target="_blank" title="go to About">Services</a> </li>
+            <li> <a href="Contact Us.html" target="_blank" title="go to About">Contact Us</a> </li>
+        </ul>
+    </div>
     <div>
         @yield('smart_content')
     </div>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 Route::get('/project', [App\Http\Controllers\ProjectController::class, 'index']) -> name("project.show");
 Route::get('/mesures/{cantine}', [App\Http\Controllers\MesuresController::class, 'index']) -> name("mesures.show");
+Route::get('/new_login', function (){
+    return view('auth.new_login');
+});
