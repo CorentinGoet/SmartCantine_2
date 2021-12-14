864b22d1a2b5249798b59f82f2a91e230b6e287d
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-12-14 14:12:46 +0100

    bugfix

--- resources/views/SmartCantine/layout_smart_cantine.blade.php
+++ resources/views/SmartCantine/layout_smart_cantine.blade.php
@@ -1,28 +1,55 @@
-<!DOCTYPE html>
-<!-- structure of project files -->
+<!doctype html>
+<html>
 <head>
+    <meta charset="UTF-8" />
     <title>SmartCantine</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href={{asset('css/mycss.css')}}>
+    <meta name="description" content="C'est la site du projet SmartCantine" />
 </head>
 <body>
-    <div class="header">
-        <h2>
-            SmartCantine
-        </h2>
-        <ul><!--c'est unorderd list -->
-            <li> <a href="test 1.html" target="_blank" title="go to test">Home</a> </li>
-            <li> <a href="About.html" target="_blank" title="go to About">About</a> </li>
-            <li><a href="Services.html" target="_blank" title="go to About">Services</a> </li>
-            <li> <a href="Contact Us.html" target="_blank" title="go to About">Contact Us</a> </li>
-        </ul>
-    </div>
-    <div>
-        @yield('smart_content')
-    </div>
-    <footer>
-        This is the footer !
-    </footer>
-</body>
+<header class=header>
+    <div class="mon-header">
+        <h1 class="titre1">SmartCantine</h1>
 
+        <h3 class="brest-metropole">Brest Métropole </h3>
+        <nav class="item1">
+            <li> <a href="About.html" target="_blank" title="la page d'accuil">Home</a> </li>
+            <li> <a href="connexion.html" target="_blank" title="se connecter">Se connecter</a> </li>
+            <li> <a href="Inscription.html" target="_blank" title="S'inscrire">Services</a> </li>
+            <li> <a href="Contact Us.html" target="_blank" title="nous connecter">s'inscrire</a> </li>
+        </nav>
+    </div>
+
+
+    <aside class=sidebar>
+        <h2 class=title-sidebar>Exemple</h2>
+    </aside>
+
+
+
+</header>
+
+<section>
+
+    <div class=text1>
+        <div class=haut_de_page>
+            <h2>Exemple</h2>
+
+
+        </div>
+
+    </div>
+
+</section>
+
+@yield('smart_content')
+<hr>
+<footer>
+    <div class=text2>
+
+        <li> <a href="test 1.html" target="_blank" title="la page d'accuil">contactez nous</a> </li>
+    </div>
+    <img class=logo src="ensta-logoh-noir.png">
+</footer>
+</body>
+</html>
