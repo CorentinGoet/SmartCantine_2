93e2c6411471c9672ed4c4725778c1927aa4dfe4
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2022-03-22 14:36:05 +0100

    removed mqtt

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,3 @@ Route::get('/new_index',  function (){
 Route::get('/api-ttn', [App\Http\Controllers\TTN_API_Controller::class, 'index']) -> name('ttn-api.show');
 
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/mqtt', [\App\Http\Controllers\MQTT_controller::class, 'index'])->name('mqtt.show');
