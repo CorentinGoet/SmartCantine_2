9bf48ea21e0107b29bc532826466d03d3cb3d891
Aziz <3z.zhtan44@gmail.com>
2022-04-20 01:36:58 +0200

    amelioration de site mesures

--- resources/views/SmartCantine/layout_smart_cantine.blade.php
+++ resources/views/SmartCantine/layout_smart_cantine.blade.php
@@ -30,12 +30,8 @@
   <!-- ======= Top Bar ======= -->
   <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
-    <!--  <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope d-flex align-items-center"><a href="SmartCantine@example.com">Smartcantine.ensta@gmail.com</a></i>
-      </div> -->
       <div class="social-links d-none d-md-flex align-items-center">
-      <!--  <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a> -->
+
       </div>
     </div>
   </section>
@@ -47,8 +43,8 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto active" href="{{url('/project#hero')}}">Home</a></li>
+          <li><a class="nav-link scrollto" href="{{url('/project#about')}}">About</a></li>
                 @if(Route::has('login'))
                     @auth
                     <li><a class="nav-link scrollto" href="{{url('/mesures/1')}}">Mesures</a></li>
@@ -58,6 +54,11 @@
                 @endif
 
     </div>
+
+
+    
+
+    
   </header><!-- End Header -->
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
@@ -69,9 +70,10 @@
       </div>
     </div>
   </section><!-- End Hero -->
+
   <main id="main">
 
-@yield('smart_content')
+    @yield('smart_content')
   </main>
 
   <!-- ======= Footer ======= -->
