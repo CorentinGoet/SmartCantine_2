67c37b856b78f70bc5ef40c0c874977121d32753
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2022-04-19 08:41:51 +0200

    bugfix

--- resources/views/SmartCantine/layout_smart_cantine.blade.php
+++ resources/views/SmartCantine/layout_smart_cantine.blade.php
@@ -2,34 +2,39 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>SmartCantine</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <title>SmartCantine</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="{{asset("css/style_template.css")}}" rel="stylesheet">
 
-  <!-- =======================================================
-
-  ======================================================== -->
+    <!-- =======================================================
+        * Template Name: BizLand - v3.7.0
+        * Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
+        * Author: BootstrapMade.com
+        * License: https://bootstrapmade.com/license/
+        ======================================================== -->
+<!-- Scripts
+    <script src={{asset("/js/main.js")}}></script> -->
     <meta charset="UTF-8" />
     <title>SmartCantine</title>
     <meta name="description" content="Website for SmartCantine project" />
@@ -37,7 +42,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="project iot noise" name="keywords">
 
-    <!--<link rel="stylesheet" href={{asset('css/mycss.css')}}> -->
+<!--<link rel="stylesheet" href={{asset('css/mycss.css')}}> -->
 
     <!-- Favicons -->
     <link href={{asset("/img/favicon.png")}} rel="icon">
@@ -62,58 +67,58 @@
 
 <body>
 
-  <!-- ======= Top Bar ======= -->
-  <section id="topbar" class="d-flex align-items-center">
+<!-- ======= Top Bar ======= -->
+<section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
-      <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope d-flex align-items-center"><a href="SmartCantine@example.com">Smartcantine.ensta@gmail.com</a></i>
-      </div>
-      <div class="social-links d-none d-md-flex align-items-center">
-        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-      </div>
+        <div class="contact-info d-flex align-items-center">
+            <i class="bi bi-envelope d-flex align-items-center"><a href="SmartCantine@example.com">Smartcantine.ensta@gmail.com</a></i>
+        </div>
+        <div class="social-links d-none d-md-flex align-items-center">
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        </div>
     </div>
-  </section>
+</section>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="d-flex align-items-center">
+<!-- ======= Header ======= -->
+<header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="{{url("project")}}">SmartCantine<span>.</span></a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
-      
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
+        <h1 class="logo"><a href="{{url("project")}}">SmartCantine<span>.</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                <li><a class="nav-link scrollto" href="#about">About</a></li>
 
                 @if(Route::has('login'))
                     @auth
-                    <li><a class="nav-link scrollto" href="{{url('/mesures/1')}}">Mesures</a></li>
+                        <li><a class="nav-link scrollto" href="{{url('/mesures/1')}}">Mesures</a></li>
 
                     @else
-                    <li><a class="nav-link scrollto" href="{{url('/login1')}}">Login</a></li>
-                    @endauth
-                @endif
-                
+                        <li><a class="nav-link scrollto" href="{{url('/login1')}}">Login</a></li>
+        @endauth
+        @endif
+
 
     </div>
 
-  </header><!-- End Header -->
+</header><!-- End Header -->
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
     <div class="container" data-aos="zoom-out" data-aos-delay="100">
-      <h1>Welcome to <span>SmartCantine</span></h1>
-      <div class="d-flex">
-        <a href="#about" class="btn-get-started scrollto">Get Started</a>
- <!--       <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> -->
-      </div>
+        <h1>Welcome to <span>SmartCantine</span></h1>
+        <div class="d-flex">
+            <a href="#about" class="btn-get-started scrollto">Get Started</a>
+            <!--       <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> -->
+        </div>
     </div>
-  </section><!-- End Hero -->
+</section><!-- End Hero -->
 
-  <main id="main">
+<main id="main">
 
 
 
@@ -121,87 +126,87 @@
 
 
 
-  <!-- ======= Footer ======= -->
- <footer id="footer">
+<!-- ======= Footer ======= -->
+    <footer id="footer">
 
- <!--   <div class="footer-newsletter">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6">
-            <h4>Join Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-        </div>
-      </div>
-    </div> -->
+        <!--   <div class="footer-newsletter">
+             <div class="container">
+               <div class="row justify-content-center">
+                 <div class="col-lg-6">
+                   <h4>Join Our Newsletter</h4>
+                   <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                   <form action="" method="post">
+                     <input type="email" name="email"><input type="submit" value="Subscribe">
+                   </form>
+                 </div>
+               </div>
+             </div>
+           </div> -->
 
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>SmartCantine<span>.</span></h3>
-            <p>
-              ENSTA Bretagne <br>
-              2 rue François Verny <br>
-              Brest, 29200<br>
-              France <br><br>
-              <strong>Email:</strong> Smartcantine.ensta@gmail.com<br>
-            </p>
-          </div>
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h3>SmartCantine<span>.</span></h3>
+                        <p>
+                            ENSTA Bretagne <br>
+                            2 rue François Verny <br>
+                            Brest, 29200<br>
+                            France <br><br>
+                            <strong>Email:</strong> Smartcantine.ensta@gmail.com<br>
+                        </p>
+                    </div>
 
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-            </ul>
-          </div>
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                        </ul>
+                    </div>
 
 
-          <!--<div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Social Networks</h4>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-            
+                    <!--<div class="col-lg-3 col-md-6 footer-links">
+                      <h4>Our Social Networks</h4>
+                      <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+                      <div class="social-links mt-3">
+                        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+
+                      </div>
+                    </div> -->
+
+                </div>
             </div>
-          </div> -->
-
         </div>
-      </div>
-    </div>
 
-    <div class="container py-4">
-      <div class="copyright">
-      <div class="credits">
+        <div class="container py-4">
+            <div class="copyright">
+                <div class="credits">
 
-      </div>
-    </div>
-  </footer><!-- End Footer -->
+                </div>
+            </div>
+    </footer><!-- End Footer -->
 
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
 
 
 
