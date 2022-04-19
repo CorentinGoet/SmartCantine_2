712877e433cc1996dca7cf0e69fdd0b91a4352b4
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2022-04-19 10:40:48 +0200

    bugfix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,37 +27,7 @@
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
 
-    <!-- =======================================================
-
-    ======================================================== -->
-
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-    <title>SmartCantine</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
-
-    <!-- Favicons -->
-    <link href="{{asset("img/favicon.png")}}" rel="icon">
-    <link href={{asset("img/apple-touch-icon.png")}} rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-    <link rel="stylesheet" href={{asset('css/login.css')}}>
-
-
-    <!-- Vendor CSS Files -->
-    <link href="{{asset('vendor/aos/aos.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
-    <link href="{{asset("css/style_template.css")}}" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
 
 
     <!-- =======================================================
